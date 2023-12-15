2dcb8e0acb2a063e58d205b08e9654f60a0ef826
Cesare Mauri <cesare.mauri@decathlon.com>
2023-12-15 14:15:33 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Homepage from './components/Homepage.vue'
+</script>
+
+<template>
+  <main>
+      <Homepage msg="You did it!"/>
+  </main>
+
+</template>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
+
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+}
+</style>
